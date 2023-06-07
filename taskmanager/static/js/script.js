e4215bdbb996972071bb9a984bb607bf2463998a
Dylan <Dylanmcmahon187@gmail.com>
2023-06-07 14:49:49 +0000

    Add tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(elems, options);
+    M.Sidenav.init(sidenav);
+}); // sidenav initialization
+
+document.addEventListener('DOMContentLoaded', function () {
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    }); // datepicker initialization
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+}); // 
